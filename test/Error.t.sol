67dc283926a0c372327b884cd655e964478bd8ec
sergiu.corches <corchessergiu194@gmail.com>
2024-02-04 20:39:25 +0200

    test for events

--- test/Error.t.sol
+++ test/Error.t.sol
@@ -20,21 +20,21 @@ contract ErrorTest is Test {
         err.throwError();
     }
 
-    // function testRequireMessage() public {
-    //     vm.expectRevert(bytes("not authorized"));
-    //     err.throwError();
-    // }
+    function testRequireMessage() public {
+        vm.expectRevert(bytes("not authorized"));
+        err.throwError();
+    }
 
-    // function testCustomError() public {
-    //     vm.expectRevert(Error.NotAuthorized.selector);
-    //     err.throwCustomError();
-    // }
+    function testCustomError() public {
+        vm.expectRevert(Error.NotAuthorized.selector);
+        err.throwCustomError();
+    }
 
-    // function testErrorLabel() public {
-    //     assertEq(uint256(1), uint256(1), "test 1");
-    //     assertEq(uint256(1), uint256(1), "test 2");
-    //     assertEq(uint256(1), uint256(1), "test 3");
-    //     assertEq(uint256(1), uint256(1), "test 4");
-    //     assertEq(uint256(1), uint256(1), "test 5");
-    // }
+    function testErrorLabel() public {
+        assertEq(uint256(1), uint256(1), "test 1");
+        assertEq(uint256(1), uint256(1), "test 2");
+        assertEq(uint256(1), uint256(1), "test 3");
+        assertEq(uint256(1), uint256(1), "test 4");
+        assertEq(uint256(1), uint256(1), "test 5");
+    }
 }
